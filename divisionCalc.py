38694cb4312515c7d36a48a3ae6514aef8cf6c31
Riddle6 <148794251+Riddle6@users.noreply.github.com>
2025-01-22 14:21:19 +0000

    Scopes & div Calc

---
+++ divisionCalc.py
@@ -0,0 +1,7 @@
+# Example Program: Division Calculator
+
+numerator = int(input("Enter the numerator: "))
+denominator = int(input("Enter the denominator: "))
+
+result = numerator / denominator
+print(f"The result is {result}")
